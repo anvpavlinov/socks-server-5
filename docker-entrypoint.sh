bb3383c724edc0593ebfc8f28c6774e74dcac64f
anvpavlinov <anvpavlinov@gmail.com>
2020-03-04 16:24:39 +0300

    Revert "Dockerfile and docker-entrypoint.sh changed for minishift build."
    This reverts commit fc51cd4a81f471ee7545ae145d9c3dd6b6a98e54.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ss5 -t
+if [ `id -u` -ge 10000 ]; then
+	cp /etc/passwd /tmp/passwd
+	cat /tmp/passwd | sed -e "s/^ss5:.*$/ss5:x:`id -u`:`id -g`::\/home\/ss5:\/sbin\/nologin/g" > /etc/passwd
+	rm /tmp/passwd
+fi
+
+ss5 -t -u ss5
 
 trap "kill `cat /var/run/ss5/ss5.pid`; exit" KILL INT
 
